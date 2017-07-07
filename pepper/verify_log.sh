a4c026a9986b670263f91e89ab2bfb62084441cd
cyubachi <cyubachi@gmail.com>
2017-07-07 18:40:46 +0900

    modify verify log script.

--- pepper/verify_log.sh
+++ pepper/verify_log.sh
@@ -2,18 +2,18 @@
 
 # ログの行をそのまま引数にもらう
 
+cd `dirname $0`
+
 proof_file_name=$1 
-restore_verify_files_arg=$*
 
 
-# logging.inputsとlogging.outputsを復元
-#echo "bin/pepper_prover_logging restore_verify_file $restore_verify_files_arg"
-sh -c "bin/pepper_prover_logging restore_verify_file $restore_verify_files_arg" 
-
-# logging.proofを復元
+# logging.inputs,logging.outputs,logging.proofを置く
+cp logs/inputs/${proof_file_name}.inputs prover_verifier_shared/logging.inputs
+cp logs/outputs/${proof_file_name}.outputs prover_verifier_shared/logging.outputs
 cp logs/proofs/${proof_file_name}.proof prover_verifier_shared/logging.proof
 
 # verifierを実行
+bin/pepper_verifier_logging verify logging.vkey logging.inputs logging.outputs logging.proof 
 bin/pepper_verifier_logging verify logging.vkey logging.inputs logging.outputs logging.proof | tail -1 | grep SUCCESS > /dev/null 2>&1
 
 exit $?
