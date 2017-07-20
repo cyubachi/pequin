f813fc66461a7119404ab95ad7c4dc0902b0a685
cyubachi <cyubachi@gmail.com>
2017-07-20 10:25:29 +0000

    change upload script

--- pepper/restore_input_and_output_all.sh
+++ pepper/restore_input_and_output_all.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-for i in `seq 1 50`
+RECORD_NUM=`wc -l logs/car_data.log | cut -f1 -d' '`
+#RECORD_NUM=50
+
+for i in `seq 1 $RECORD_NUM`
 do
   args=`head -${i} logs/car_data.log  | tail -1 | sed 's/,/ /g'`
   bash restore_input_and_output.sh $args
