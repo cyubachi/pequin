93cd16c2a001de66c954b9d3bbd962d3e60f9593
cyubachi <cyubachi@gmail.com>
2017-08-29 18:58:07 +0900

    change input and output save path.

--- pepper/restore_input_and_output.sh
+++ pepper/restore_input_and_output.sh
@@ -2,15 +2,16 @@
 
 # ログの行をそのまま引数にもらう
 
-proof_file_name=$1 
+car_id=$1 
+proof_file_name=$2 
 restore_verify_files_arg=$*
 
 
 # logging.inputsとlogging.outputsを復元
 sh -c "bin/pepper_verifier_logging restore_verify_file $restore_verify_files_arg" 
 
-mkdir -p logs/inputs
-mkdir -p logs/outputs
+mkdir -p logs/inputs/${car_id}
+mkdir -p logs/outputs/${car_id}
 
-mv prover_verifier_shared/logging.inputs logs/inputs/${proof_file_name}.inputs
-mv prover_verifier_shared/logging.outputs logs/outputs/${proof_file_name}.outputs
+mv prover_verifier_shared/logging.inputs logs/inputs/${car_id}/${proof_file_name}.inputs
+mv prover_verifier_shared/logging.outputs logs/outputs/${car_id}/${proof_file_name}.outputs
