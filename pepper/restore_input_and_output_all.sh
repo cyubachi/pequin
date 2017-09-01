cbbf87517aa67dbe35f9755f98b1cfd9c0d148a7
cyubachi <cyubachi@gmail.com>
2017-09-01 20:45:21 +0900

    fix file bug server side.

--- pepper/restore_input_and_output_all.sh
+++ pepper/restore_input_and_output_all.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-RECORD_NUM=`wc -l logs/car_data.log | cut -f1 -d' '`
-#RECORD_NUM=50
-
-for i in `seq 1 $RECORD_NUM`
+for f in `ls -1 ~/pequin/pepper/logs/car_data/*/append_car_data.log`
 do
-  args=`head -${i} logs/car_data.log  | tail -1 | sed 's/,/ /g'`
-  bash restore_input_and_output.sh $args
+  RECORD_NUM=`wc -l ${f} | cut -f1 -d' '`
+  for i in `seq 1 $RECORD_NUM`
+  do
+    args=`head -${i} ${f} | tail -1 | sed 's/,/ /g'`
+    bash restore_input_and_output.sh $args
+  done
 done
